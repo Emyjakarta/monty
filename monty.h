28622d9772601978f7bcdd1374f878314dcb7d5c
Emyjakarta <ukonud4@gmail.com>
2023-12-15 18:40:59 +0100

    Added a Makefile and bash script for installation

--- monty.h
+++ monty.h
@@ -1,10 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define _GNU_SOURCE
 
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
 #define BUFF_SIZE 1024
 
