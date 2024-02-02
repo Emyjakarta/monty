7a7efeecd05361d624117a23d6786b4043ea6546
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-02 17:41:22 +0000

    Updated function documentations

--- arithmetic_operations.c
+++ arithmetic_operations.c
@@ -4,7 +4,6 @@
  * add-adds the top two elements of the stack
  * @stack: pointer to a pointer to the stack
  * @line_number: line number
- * Return: void
  */
 void add(stack_t **stack, unsigned int line_number)
 {
@@ -15,18 +14,16 @@ void add(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		handle_exit();
 	}
+
 	top_element = top(monty_list);
 	pop(stack, line_number);
 	(*stack)->n += top_element;
-
 }
 
 /**
- * sub-subtracts the top element of the stack
- * from the second top element of the stack
+ * sub - subtracts the top two elements of the stack
  * @stack: pointer to a pointer to the stack
  * @line_number: line number
- * Return: void
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
@@ -37,18 +34,16 @@ void sub(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		handle_exit();
 	}
+
 	top_element = top(monty_list);
 	pop(stack, line_number);
 	(*stack)->n -= top_element;
-
 }
 
 /**
- * mul-multiplies the second top element of
- * the stack with the top element of the stack
+ * mul - multiplies the top two elements of the stack
  * @stack: pointer to a pointer to the stack
  * @line_number: line number
- * Return: void
  */
 void mul(stack_t **stack, unsigned int line_number)
 {
@@ -59,18 +54,16 @@ void mul(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		handle_exit();
 	}
+
 	top_element = top(monty_list);
 	pop(stack, line_number);
 	(*stack)->n *= top_element;
-
 }
 
 /**
- * division-divides the second top element of
- * the stack by the top element of the stack
+ * division - divides the top two elements of the stack
  * @stack: pointer to a pointer to the stack
  * @line_number: line number
- * Return: void
  */
 void division(stack_t **stack, unsigned int line_number)
 {
@@ -81,24 +74,23 @@ void division(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		handle_exit();
 	}
+
 	top_element = top(monty_list);
 	if (top_element == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
 		handle_exit();
 	}
+
 	pop(stack, line_number);
 	(*stack)->n /= top_element;
-
 }
 
 /**
- * mod-computes the rest of the division of the
- * second top element of the stack by the
+ * mod - performs modulo arithmetic on the top two elements of the stack
  * top element of the stack
  * @stack: pointer to a pointer to the stack
  * @line_number: line number
- * Return: void
  */
 void mod(stack_t **stack, unsigned int line_number)
 {
@@ -109,13 +101,14 @@ void mod(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 		handle_exit();
 	}
+
 	top_element = top(monty_list);
 	if (top_element == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
 		handle_exit();
 	}
+
 	pop(stack, line_number);
 	(*stack)->n %= top_element;
-
 }
